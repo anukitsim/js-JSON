09e25bb04dd55b5e58c5b154442bd68858c0f12d
anukitsim <104069419+anukitsim@users.noreply.github.com>
2022-06-20 17:44:18 +0400

    done

---
+++ script.js
@@ -0,0 +1,32 @@
+const result = document.getElementById("res")
+
+const list = []
+
+datas ()
+
+async function datas () {
+    const res = await fetch ("https://randomuser.me/api?results=50")
+
+    const { results } = await res.json()
+
+    result.innerHTML = ''
+
+    results.forEach (user => {
+        const li = document.createElement("li")
+
+        list.push(li)
+
+        li.innerHTML = `
+        <img src="${user.picture.medium}" alt="${user.name.first}">
+        <div class="user-info">
+        <h4>${user.name.first} ${user.name.last} </h4>
+        <p>${user.email}</p>
+
+        <p>${user.location.city}, ${user.location.country}</p>
+        </div>
+        `
+        result.appendChild(li)
+    })
+
+}
+
